84e3591b5f3a762843cc7308ce0491dad7d1c45c
Nathan Waterman <nathan.waterman99@gmail.com>
2015-04-17 00:17:40 -0400

    portfolio script commit

--- js/portfolioScript.js
+++ js/portfolioScript.js
@@ -93,14 +93,14 @@ $(document).scroll(function(){
         var topDist = $(".container").position();
         if (scroll > topDist.top) {
             $('.navbar').css({"position":"fixed","top":"0","z-index":"100"});
-			//$('.logo,.name,.title').hide();
+			$('.logo,.name,.title').hide();
 			$('.logo,.name,.title').css({"z-index":"-1"});
 			console.log("navbar fixed");
 			console.log("logo name title hidden");
         } else {
 			//$('.scrollBar').css({"position":"fixed","bottom":"0px"});
             $('.navbar').css({"position":"absolute","top":"auto","z-index":"100"});
-			//$('.logo,.name,.title').show();
+			$('.logo,.name,.title').show();
 			$('.logo,.name,.title').css({"z-index":"1"});
 			console.log("navbar absolute");
 			console.log("logo name title show");
