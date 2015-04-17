78d99822d28c7167d1dfef1328d26435d99f3ddb
NathanWaterman <nathan.waterman99@gmail.com>
2015-04-17 15:40:17 -0400

    commit

--- js/portfolioScript.js
+++ js/portfolioScript.js
@@ -246,7 +246,7 @@ function scrollAnimate(){
 			console.log("clickB");
 			});
 		//newport button
-		$('.newport-content-btn').delegate('click',function(event){
+		$('.newportLoggedIn').on('click','.newport-content-btn',function(event){
 			$('html, body').animate({
         		scrollTop: $('#newport-content').offset().top - 25
     		}, 2000);
