4befdd47c070f20c518f6cf2404e444de1311a16
Nathan Waterman <nathan.waterman99@gmail.com>
2015-04-20 13:14:13 -0400

    commit

--- js/portfolioScript.js
+++ js/portfolioScript.js
@@ -43,12 +43,12 @@ function findOrientation(){
 		//portrait
 			if(window.orientation == 0){
 				$('.logo,.name,.title').show();
-				$('div.parallax_wrapper').css("height","960px");
+				//$('div.parallax_wrapper').css("height","960px");
 			}
 			//landscape
 			else if(window.orientation == 90){
 				$('.name,.title').hide();
-				$('div.parallax_wrapper').css("height","703px");
+				//$('div.parallax_wrapper').css("height","703px");
 				console.log("landscape");
 				}	
 		}, false);
